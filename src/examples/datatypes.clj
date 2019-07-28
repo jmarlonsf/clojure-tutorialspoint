e3e53d88e84736651bce3f02883e5582f3b8bcb3
João Marlon <marlonsouto@gmail.com>
2019-07-27 22:54:08 -0300

    datatypes

---
+++ src/examples/datatypes.clj
@@ -0,0 +1,24 @@
+(ns examples.datatypes
+  (:gen-class))
+
+;This program display Hello World
+(defn Example []
+  ;the below code declares a integer variable
+  (def x 1)
+
+  ;the below code declares a float variable
+  (def y 1.25)
+
+  ;the below code declares a string variable
+  (def str1 "Hello")
+
+  ;the below code declares a boolean variable
+  (def status true)
+  (def STATUS false)
+
+  (println x)
+  (println y)
+  (println str1)
+  (println status)
+  (println STATUS))
+(Example)
